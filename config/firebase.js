cbd459aea7457df9028fd4a9108f03b499b51b0e
Raheel Ali <ytpfm2739@gmail.com>
2026-04-08 23:24:32 +0400

    Initial project setup with Expo and Firebase

---
+++ config/firebase.js
@@ -0,0 +1,18 @@
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCUDrx_eILIQfrPgLlL7geU-Z6KzsIBHvg",
+  authDomain: "finmanager-989a6.firebaseapp.com",
+  projectId: "finmanager-989a6",
+  storageBucket: "finmanager-989a6.firebasestorage.app",
+  messagingSenderId: "561021736486",
+  appId: "1:561021736486:web:ddbd68ccd7732c7c8738e7",
+  measurementId: "G-XTW7VGNF9M",
+};
+
+const app = initializeApp(firebaseConfig);
+
+export const auth = getAuth(app);
+export const db = getFirestore(app);
